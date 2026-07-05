8f09ecdb78558cb11c34106039fed180ce0458df
janani manivel <poorani.mani303@gmail.com>
2026-07-05 16:53:53 +0530

    fixed gemini api key ;-;

--- gemini.py
+++ gemini.py
@@ -7,7 +7,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 from google import genai
       
-client_ai = genai.Client(api_key="GEMINI_API_KEY")
+client_ai = genai.Client(api_key=GEMINI_API_KEY)
 
 def ask_gemini(prompt):
   response = client_ai.models.generate_content(
